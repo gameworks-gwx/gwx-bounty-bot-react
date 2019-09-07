fb3f44fe3f453d6066dd07f773269b1c816b6062
ThanMatt <belphegorous@gmail.com>
2019-09-07 17:32:57 +0800

    Added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import 'semantic-ui-css/semantic.min.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <p>Boo</p>
       </div>
     );
   }
