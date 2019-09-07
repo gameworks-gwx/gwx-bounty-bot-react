df54d64d99532eda95838c3ca603d1dc580b7e72
ThanMatt <belphegorous@gmail.com>
2019-09-07 17:29:24 +0800

    First Commit

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,17 @@
+import * as actionTypes from 'react';
+import updateObject from '../utility';
+
+const initialState = {
+  foo: ''
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    //!! Cases
+
+    default:
+      return state
+  }
+}
+
+export default reducer;
