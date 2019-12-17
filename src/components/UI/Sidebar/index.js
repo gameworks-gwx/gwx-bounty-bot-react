91d2a0c1dd58f63effe3b0ac11689bf24f69ff10
Aethan Matthew Ilagan <aethanilagan@gmail.com>
2019-12-17 17:39:45 +0800

    Sidebar and home dashboard fixes

--- src/components/UI/Sidebar/index.js
+++ src/components/UI/Sidebar/index.js
@@ -1,128 +1,214 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Menu, Icon, PageHeader } from 'antd'
-import Responsive from '../Responsive'
-import image from '../../../assets/img/gwx-logo.png'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Menu, Icon, PageHeader } from "antd";
+import Responsive from "../Responsive";
+import image from "../../../assets/img/gwx-logo.png";
 const { SubMenu } = Menu;
 
 const Sidebar = ({ pathname, closeDrawer, email }) => {
   return (
     <>
       <Responsive device="pc">
-        <Menu data-test="menuComponent" selectedKeys={[pathname]} mode="inline" style={{ height: '100vh', width: '29vh' }}>
-          <PageHeader avatar={{ src: image }} title="Gameworks Bounty" size={{ fontSize: 10 }} subTitle={email ? 'Logged in as ' + email : null} />
-          <Menu.Item data-test="pcMenuItem" key="/" onClick={closeDrawer ? closeDrawer : ''}>
-            <NavLink to={{
-              pathname: '/',
-              state: {
-                pageTitle: 'Home'
-              }
-            }}>
+        <Menu
+          data-test="menuComponent"
+          selectedKeys={[pathname]}
+          mode="inline"
+          style={{ height: "100vh", width: "35vh" }}
+        >
+          <PageHeader
+            avatar={{ src: image, size: "small" }}
+            title="Gameworks Bounty"
+            subTitle={email ? "Logged in as " + email : null}
+          />
+          <Menu.Item
+            data-test="pcMenuItem"
+            key="/"
+            onClick={closeDrawer ? closeDrawer : ""}
+          >
+            <NavLink
+              to={{
+                pathname: "/",
+                state: {
+                  pageTitle: "Home"
+                }
+              }}
+            >
               <Icon type="home" />
               Home
-        </NavLink>
+            </NavLink>
           </Menu.Item>
 
-          <Menu.Item data-test="pcMenuItem" key="/verifications" onClick={closeDrawer ? closeDrawer : ''}>
-            <NavLink to={{
-              pathname: '/verifications',
-              state: {
-                pageTitle: 'Verifications'
-              }
-            }}>
+          <Menu.Item
+            data-test="pcMenuItem"
+            key="/verifications"
+            onClick={closeDrawer ? closeDrawer : ""}
+          >
+            <NavLink
+              to={{
+                pathname: "/verifications",
+                state: {
+                  pageTitle: "Verifications"
+                }
+              }}
+            >
               <Icon type="camera" />
               Verifications
-        </NavLink>
+            </NavLink>
           </Menu.Item>
-          <SubMenu title={<span><Icon type="table" />Dashboard</span>}>
-            <Menu.Item data-test="pcMenuItem" key="/airdrop/1" onClick={closeDrawer ? closeDrawer : ''}>
-              <NavLink to={{
-                pathname: '/airdrop/1',
-                state: {
-                  pageTitle: 'Airdrop Dashboard'
-                }
-              }}>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="table" />
+                Dashboard
+              </span>
+            }
+          >
+            <Menu.Item
+              data-test="pcMenuItem"
+              key="/airdrop/1"
+              onClick={closeDrawer ? closeDrawer : ""}
+            >
+              <NavLink
+                to={{
+                  pathname: "/airdrop/1",
+                  state: {
+                    pageTitle: "Airdrop Dashboard"
+                  }
+                }}
+              >
                 Airdrop Dashboard
-          </NavLink>
+              </NavLink>
             </Menu.Item>
-
           </SubMenu>
 
-          <SubMenu title={<span><Icon type="user" /><span>Configuration</span></span>}>
-
-            <Menu.Item data-test="pcMenuItem" key="/administrators" onClick={closeDrawer ? closeDrawer : ''}>
-              <NavLink to={{
-                pathname: '/administrators',
-                state: {
-                  pageTitle: 'Administrators'
-                }
-              }}>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="user" />
+                <span>Configuration</span>
+              </span>
+            }
+          >
+            <Menu.Item
+              data-test="pcMenuItem"
+              key="/administrators"
+              onClick={closeDrawer ? closeDrawer : ""}
+            >
+              <NavLink
+                to={{
+                  pathname: "/administrators",
+                  state: {
+                    pageTitle: "Administrators"
+                  }
+                }}
+              >
                 Administrators
-          </NavLink>
+              </NavLink>
             </Menu.Item>
           </SubMenu>
-
-        </Menu >
+        </Menu>
       </Responsive>
 
       <Responsive device="mobile">
-        <Menu selectedKeys={[pathname]} mode="inline" style={{ height: '100vh', width: '16rem' }}>
-          <PageHeader title="Gameworks Bounty" subTitle={email ? 'Logged in as ' + email : null} />
-          <Menu.Item data-test="mobileMenuItem" key="/" onClick={closeDrawer ? closeDrawer : ''}>
-            <NavLink to={{
-              pathname: '/',
-              state: {
-                pageTitle: 'Home'
-              }
-            }}>
+        <Menu
+          selectedKeys={[pathname]}
+          mode="inline"
+          style={{ height: "100vh", width: "16rem" }}
+        >
+          <PageHeader
+            title="Gameworks Bounty"
+            subTitle={email ? "Logged in as " + email : null}
+          />
+          <Menu.Item
+            data-test="mobileMenuItem"
+            key="/"
+            onClick={closeDrawer ? closeDrawer : ""}
+          >
+            <NavLink
+              to={{
+                pathname: "/",
+                state: {
+                  pageTitle: "Home"
+                }
+              }}
+            >
               <Icon type="home" />
               Home
-        </NavLink>
+            </NavLink>
           </Menu.Item>
 
-          <Menu.Item data-test="mobileMenuItem" key="/verifications" onClick={closeDrawer ? closeDrawer : ''}>
-            <NavLink to={{
-              pathname: '/verifications',
-              state: {
-                pageTitle: 'Verifications'
-              }
-            }}>
+          <Menu.Item
+            data-test="mobileMenuItem"
+            key="/verifications"
+            onClick={closeDrawer ? closeDrawer : ""}
+          >
+            <NavLink
+              to={{
+                pathname: "/verifications",
+                state: {
+                  pageTitle: "Verifications"
+                }
+              }}
+            >
               <Icon type="camera" />
               Verifications
-        </NavLink>
+            </NavLink>
           </Menu.Item>
-          <SubMenu title={<span><Icon type="table" />Dashboard</span>}>
-            <Menu.Item data-test="mobileMenuItem" key="/airdrop/1" onClick={closeDrawer ? closeDrawer : ''}>
-              <NavLink to={{
-                pathname: '/airdrop/1',
-                state: {
-                  pageTitle: 'Airdrop Dashboard'
-                }
-              }}>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="table" />
+                Dashboard
+              </span>
+            }
+          >
+            <Menu.Item
+              data-test="mobileMenuItem"
+              key="/airdrop/1"
+              onClick={closeDrawer ? closeDrawer : ""}
+            >
+              <NavLink
+                to={{
+                  pathname: "/airdrop/1",
+                  state: {
+                    pageTitle: "Airdrop Dashboard"
+                  }
+                }}
+              >
                 Airdrop Dashboard
-          </NavLink>
+              </NavLink>
             </Menu.Item>
-
           </SubMenu>
 
-          <SubMenu title={<span><Icon type="user" /><span>Configuration</span></span>}>
-
-            <Menu.Item data-test="mobileMenuItem" key="/administrators" onClick={closeDrawer ? closeDrawer : ''}>
-              <NavLink to={{
-                pathname: '/administrators',
-                state: {
-                  pageTitle: 'Administrators'
-                }
-              }}>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="user" />
+                <span>Configuration</span>
+              </span>
+            }
+          >
+            <Menu.Item
+              data-test="mobileMenuItem"
+              key="/administrators"
+              onClick={closeDrawer ? closeDrawer : ""}
+            >
+              <NavLink
+                to={{
+                  pathname: "/administrators",
+                  state: {
+                    pageTitle: "Administrators"
+                  }
+                }}
+              >
                 Administrators
-          </NavLink>
+              </NavLink>
             </Menu.Item>
           </SubMenu>
-
-        </Menu >
+        </Menu>
       </Responsive>
     </>
-  )
-}
+  );
+};
 
 export default Sidebar;
--- src/components/UI/StatisticCard/index.js
+++ src/components/UI/StatisticCard/index.js
@@ -1,56 +1,94 @@
-import React from 'react';
-import { Card, Statistic, Col } from 'antd'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Card, Statistic, Col, Divider, Row } from "antd";
+import { Link } from "react-router-dom";
 
 const { Meta } = Card;
 
-const StatisticCard = ({ cardTitle, pathname, statisticData, size, loading }) => {
+const StatisticCard = ({
+  cardTitle,
+  pathname,
+  statisticData,
+  size,
+  loading,
+  className
+}) => {
   let pageTitle;
 
-  if (pathname === '/') {
-    pageTitle = 'Home'
-  } else if (pathname === '/airdrop/1') {
-    pageTitle = 'Airdrop Dashboard'
-  } else if (pathname === '/administrators') {
-    pageTitle = 'Administrators'
-  } else if (pathname === '/verifications') {
-    pageTitle = 'Verifications'
+  if (pathname === "/") {
+    pageTitle = "Home";
+  } else if (pathname === "/airdrop/1") {
+    pageTitle = "Airdrop Dashboard";
+  } else if (pathname === "/administrators") {
+    pageTitle = "Administrators";
+  } else if (pathname === "/verifications") {
+    pageTitle = "Verifications";
   }
 
   return (
-    <Link to={{
-      pathname,
-      state: {
-        pageTitle
-      }
-    }}>
-      <Card className="statistic-card" loading={loading} size={size ? size : ''} hoverable>
-        <Meta title={cardTitle} style={{ marginBottom: '1rem' }} size="small" />
-        {
-          statisticData.length
-            ? statisticData.map((data, index) => {
-              return (
-                <Col span={24 / statisticData.length} key={index}>
-                  <Statistic
-                    title={data.title}
-                    value={data.value}
-                    valueStyle={{ fontSize: '4rem', textAlign: 'center' }}
-                    style={{ textAlign: 'center' }}
-                  />
-                </Col>
-              )
-            })
-            :
-            <Statistic
-              title={statisticData.title}
-              value={statisticData.value}
-              valueStyle={{ fontSize: '4rem', textAlign: 'center' }}
-              style={{ textAlign: 'center' }}
-            />
+    <Link
+      to={{
+        pathname,
+        state: {
+          pageTitle
         }
+      }}
+    >
+      <Card
+        className={className}
+        loading={loading}
+        size={size ? size : ""}
+        hoverable={true}
+      >
+        <Meta title={cardTitle} style={{ marginBottom: "1rem" }} size="small" />
+        {statisticData.length ? (
+          <>
+            <Row type="flex" justify="space-around">
+              {statisticData.map((data, index) => {
+                return (
+                  <>
+                    {index !== 0 ? (
+                      <>
+                        <Col>
+                          <Divider type="vertical" style={{ height: 100 }} />
+                        </Col>
+                        <Col key={index}>
+                          <Statistic
+                            title={data.title}
+                            value={data.value}
+                            valueStyle={{
+                              fontSize: "3rem",
+                              textAlign: "center"
+                            }}
+                            style={{ textAlign: "center" }}
+                          />
+                        </Col>
+                      </>
+                    ) : (
+                      <Col key={index}>
+                        <Statistic
+                          title={data.title}
+                          value={data.value}
+                          valueStyle={{ fontSize: "3rem", textAlign: "center" }}
+                          style={{ textAlign: "center" }}
+                        />
+                      </Col>
+                    )}
+                  </>
+                );
+              })}
+            </Row>
+          </>
+        ) : (
+          <Statistic
+            title={statisticData.title}
+            value={statisticData.value}
+            valueStyle={{ fontSize: "3rem", textAlign: "center" }}
+            style={{ textAlign: "center" }}
+          />
+        )}
       </Card>
-    </Link >
-  )
-}
+    </Link>
+  );
+};
 
-export default StatisticCard
+export default StatisticCard;
